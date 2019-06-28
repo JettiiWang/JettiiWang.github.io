2270062884cc5c8c8d41ff74cd129a2acf4eceec
wangzhenjie <1241480460@qq.com>
2019-06-28 13:05:25 +0800

    test001

--- wxs/src/router/index.js
+++ wxs/src/router/index.js
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: () => import('@/views/index/index.vue')
     }
   ]
 })
