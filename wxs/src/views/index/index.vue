cc2ad5639aa7eb0c19f7402dacf825393dda802b
wangzhenjie <1241480460@qq.com>
2019-06-28 13:09:17 +0800

    test001 (补充)

---
+++ wxs/src/views/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class='index'>
+        <garden/>  
+    </div>
+</template>
+<script>
+import garden from '@/components/cursor/garden'
+export default {
+    components:{
+        garden
+    },
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+    }
+}
+</script>
+<style lang="scss" scoped>
+.index {
+    width :100%;
+    height :100%;
+}
+    
+
+
+    
+</style>
+
